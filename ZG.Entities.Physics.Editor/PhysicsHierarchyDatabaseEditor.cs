2805ac1cec36d60ce9ba4c284b9650761e05106a
linfuqing <linfuqing@126.com>
2023-09-27 12:42:47 +0800

    Update PhysicsHierarchyDatabaseEditor.cs

--- ZG.Entities.Physics.Editor/PhysicsHierarchyDatabaseEditor.cs
+++ ZG.Entities.Physics.Editor/PhysicsHierarchyDatabaseEditor.cs
@@ -6,6 +6,63 @@ namespace ZG
     [CustomEditor(typeof(PhysicsHierarchyDatabase))]
     public class PhysicsHierarchyDatabaseEditor : Editor
     {
+        [MenuItem("Assets/ZG/MeshInstance/Rebuild All Physics Hierarchies")]
+        [CommandEditor("Physics", 0)]
+        public static void RebuildAllPhysicsHierarchies()
+        {
+            PhysicsHierarchyDatabase target;
+            var guids = AssetDatabase.FindAssets("t:PhysicsHierarchyDatabase");
+            string path;
+            int numGUIDs = guids.Length;
+            for (int i = 0; i < numGUIDs; ++i)
+            {
+                path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (EditorUtility.DisplayCancelableProgressBar("Rebuild All Physics Hierarchies", path, i * 1.0f / numGUIDs))
+                    break;
+
+                target = AssetDatabase.LoadAssetAtPath<PhysicsHierarchyDatabase>(path);
+                if (target == null)
+                    continue;
+
+                if (target.root == null)
+                {
+                    Debug.LogError($"{target.name} missing root", target.root);
+
+                    continue;
+                }
+
+                switch (PrefabUtility.GetPrefabAssetType(target.root))
+                {
+                    case PrefabAssetType.Regular:
+                    case PrefabAssetType.Variant:
+                        var root = (Transform)PrefabUtility.InstantiatePrefab(target.root);
+
+                        PrefabUtility.RecordPrefabInstancePropertyModifications(root);
+
+                        var prefab = PrefabUtility.GetNearestPrefabInstanceRoot(root);
+
+                        PrefabUtility.ApplyPrefabInstance(prefab, InteractionMode.AutomatedAction);
+
+                        DestroyImmediate(prefab);
+                        break;
+                }
+
+                //PhysicsHierarchyDatabase.Data.isShowProgressBar = false;
+                try
+                {
+                    target.Create();
+
+                    target.EditorMaskDirty();
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogException(e.InnerException ?? e, target);
+                }
+            }
+
+            EditorUtility.ClearProgressBar();
+        }
+
         public override void OnInspectorGUI()
         {
             var target = (PhysicsHierarchyDatabase)base.target;
